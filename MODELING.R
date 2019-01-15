77719dde5fb5d1208ac1806fe07a10558762e08b
marioguevara <mguevara@udel.edu>
2019-01-15 11:56:21 -0500

    Update MODELING.R

--- MODELING.R
+++ MODELING.R
@@ -22,9 +22,36 @@ x <- as(covs,'SpatialPixelsDataFrame')
 x@data[] <- lapply(x@data, NA2mean)
 covs <- raster::stack(covs)
 
+#AUTOCORRELACION ESPACIAL
+dat <- training
+coordinates(dat) <- ~ x + y
+class(dat)
+dat@proj4string <- CRS(projargs = "+init=epsg:4326")
+dat@proj4string
+library(raster)
 
 
+datdf <- dat@data
+datdf <- datdf[, c("ARENA", names(covs))]
+# Fit a multiple linear regression model between the log transformed
+# values of ARENA and the top 20 covariates
+model.MLR <- lm(log(ARENA) ~ ., data = datdf)
+# stepwise variable selection
+model.MLR.step <- step(model.MLR, direction="both")
+# summary and anova of the new model using stepwise covariates
+# selection
+summary(model.MLR.step)
+anova(model.MLR.step)
 
+# Graphical diagnosis of the regression analysis
+par(mfrow=c(2,2))
+plot(model.MLR.step)
+
+# Project point data
+dat <- spTransform(dat, CRS("+init=epsg:6204"))
+# Project covariates to VN-2000 UTM 48N
+covs <- projectRaster(covs, crs = CRS("+init=epsg:6204"),
+method='ngb')
 
 
 
